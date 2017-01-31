c598d0510c015b8860727fee44505de851304cc7
Joakim Berglund <joakim.berglund@hm.com>
2017-01-31 15:21:31 +0100

    had the wrong idea

--- src/main/java/se/su/thesis/Recognizer.java
+++ src/main/java/se/su/thesis/Recognizer.java
@@ -1,25 +1,7 @@
 package se.su.thesis;
 
-import org.bytedeco.javacv.*;
-import org.bytedeco.javacpp.*;
-import org.bytedeco.javacpp.indexer.*;
-import static org.bytedeco.javacpp.opencv_core.*;
-import static org.bytedeco.javacpp.opencv_imgproc.*;
-import static org.bytedeco.javacpp.opencv_calib3d.*;
-import static org.bytedeco.javacpp.opencv_objdetect.*;
-
 public class Recognizer {
 
-    // TODO: Set this, in the example they use args
-    private String classifierName = null;
-
     public void recognize() {
-        Loader.load(opencv_objdetect.class);
-
-        CvHaarClassifierCascade classifier = new CvHaarClassifierCascade(cvLoad(classifierName));
-        if(classifier.isNull()) {
-            System.err.println("Error loading classifier file:" + classifierName);
-            System.exit(1);
-        }
     }
 }
