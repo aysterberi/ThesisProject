b026f1d5d1119d388b3c2610fe0513e50586da82
Joakim Berglund <joakim.berglund@hm.com>
2017-02-01 08:06:45 +0100

    comment code, found non deprecated example

--- src/main/java/se/su/thesis/Recognizer.java
+++ src/main/java/se/su/thesis/Recognizer.java
@@ -1,8 +1,10 @@
 /*
- * Modified from googlecode
+ * Modified from https://github.com/bytedeco
  */
 
 package se.su.thesis;
+
+import org.bytedeco.javacpp.opencv_face;
 
 import static org.bytedeco.javacpp.opencv_core.*;
 import static org.bytedeco.javacpp.opencv_highgui.*;
@@ -16,89 +18,90 @@ import java.util.List;
 
 public class Recognizer {
 
-    // NOTE: All uses of Mat are modified because cvMat is deprecated
-    // If all else fails, try using cvMat instead.
-
-    private int numberOfTrainingFaces = 0;
-    IplImage[] trainingFaces;
-    IplImage[] testFaces;
-    Mat personNumTruthMat;
-    int numberOfPersons;
-    final List<String> personNames = new ArrayList<>();
-    int numberOfEigenValues;
-    IplImage[] eigenVectArr;
-    Mat eigenValueMat;
-    IplImage pAverageTrainImage;
-    Mat projectedTrainFaceMat;
-
-    public Recognizer() {
-    }
-
-    public void learn(final String trainingFileName) {
-        int i;
-
-        System.err.println("<---------------------------------->");
-        System.err.println("Loading training images from " + trainingFileName);
-        trainingFaces = loadFaceImageArray(trainingFileName);
-    }
-
-    private IplImage[] loadFaceImageArray(final String trainingFileName) {
-        IplImage[] faceImageArr;
-        BufferedReader imgListFile;
-        String filename;
-        int iFace = 0;
-        int numFaces = 0;
-        int i;
-
-        try {
-            imgListFile = new BufferedReader(new FileReader(trainingFileName));
-
-            // TODO: this could be adapted to get files in a different way
-            // Currently this requires us to create a csv file that list all the files
-            while (true) {
-                final String line = imgListFile.readLine();
-                if (line == null || line.isEmpty())
-                    break;
-                numFaces++;
-            }
-            System.err.println("numFaces:" + numFaces);
-            imgListFile = new BufferedReader(new FileReader(trainingFileName));
-
-            faceImageArr = new IplImage[numFaces];
-            personNumTruthMat = new Mat(1, numFaces, CV_32SC1);
-            for (int j = 0; j < numFaces; j++)
-                personNumTruthMat.put(new Mat(0, j, 0));
-
-            personNames.clear();
-            numberOfPersons = 0;
-
-            for (iFace = 0; iFace < numFaces; iFace++) {
-                String personName;
-                String sPersonName;
-                int personNumber;
-
-                final String line = imgListFile.readLine();
-                if (line.isEmpty())
-                    break;
-                final String[] tokens = line.split(" ");
-                personNumber = Integer.parseInt(tokens[0]);
-                personName = tokens[1];
-                filename = tokens[2];
-                sPersonName = personName;
-                System.err.println("Got " + iFace + " " + personNumber + " " + personName + " " + filename);
-
-                if (personNumber > numberOfPersons) {
-                    personNames.add(sPersonName);
-                    numberOfPersons = personNumber;
-                    System.err.println("Got new Person " + sPersonName + " -> numberOfPersons = " + numberOfPersons + " [" + personNames.size() + "]");
-                }
-                personNumTruthMat.put(new Mat(0, iFace, personNumber));
-                // TODO: Continue here
-                //faceImageArr[iFace] = load
-            }
-
-        } catch (IOException ioe) {
-
-        }
-    }
+//    // NOTE: All uses of Mat are modified because cvMat is deprecated
+//    // If all else fails, try using cvMat instead.
+//
+//    private int numberOfTrainingFaces = 0;
+//    IplImage[] trainingFaces;
+//    IplImage[] testFaces;
+//    Mat personNumTruthMat;
+//    int numberOfPersons;
+//    final List<String> personNames = new ArrayList<>();
+//    int numberOfEigenValues;
+//    IplImage[] eigenVectArr;
+//    Mat eigenValueMat;
+//    IplImage pAverageTrainImage;
+//    Mat projectedTrainFaceMat;
+//
+//    public Recognizer() {
+//        opencv_face.FaceRecognizer faceRecognizer = new opencv_face.LBPHFaceRecognizer();
+//    }
+//
+//    public void learn(final String trainingFileName) {
+//        int i;
+//
+//        System.err.println("<---------------------------------->");
+//        System.err.println("Loading training images from " + trainingFileName);
+//        trainingFaces = loadFaceImageArray(trainingFileName);
+//    }
+//
+//    private IplImage[] loadFaceImageArray(final String trainingFileName) {
+//        IplImage[] faceImageArr;
+//        BufferedReader imgListFile;
+//        String filename;
+//        int iFace = 0;
+//        int numFaces = 0;
+//        int i;
+//
+//        try {
+//            imgListFile = new BufferedReader(new FileReader(trainingFileName));
+//
+//            // TODO: this could be adapted to get files in a different way
+//            // Currently this requires us to create a csv file that list all the files
+//            while (true) {
+//                final String line = imgListFile.readLine();
+//                if (line == null || line.isEmpty())
+//                    break;
+//                numFaces++;
+//            }
+//            System.err.println("numFaces:" + numFaces);
+//            imgListFile = new BufferedReader(new FileReader(trainingFileName));
+//
+//            faceImageArr = new IplImage[numFaces];
+//            personNumTruthMat = new Mat(1, numFaces, CV_32SC1);
+//            for (int j = 0; j < numFaces; j++)
+//                personNumTruthMat.put(new Mat(0, j, 0));
+//
+//            personNames.clear();
+//            numberOfPersons = 0;
+//
+//            for (iFace = 0; iFace < numFaces; iFace++) {
+//                String personName;
+//                String sPersonName;
+//                int personNumber;
+//
+//                final String line = imgListFile.readLine();
+//                if (line.isEmpty())
+//                    break;
+//                final String[] tokens = line.split(" ");
+//                personNumber = Integer.parseInt(tokens[0]);
+//                personName = tokens[1];
+//                filename = tokens[2];
+//                sPersonName = personName;
+//                System.err.println("Got " + iFace + " " + personNumber + " " + personName + " " + filename);
+//
+//                if (personNumber > numberOfPersons) {
+//                    personNames.add(sPersonName);
+//                    numberOfPersons = personNumber;
+//                    System.err.println("Got new Person " + sPersonName + " -> numberOfPersons = " + numberOfPersons + " [" + personNames.size() + "]");
+//                }
+//                personNumTruthMat.put(new Mat(0, iFace, personNumber));
+//                // TODO: Continue here
+//                //faceImageArr[iFace] = load
+//            }
+//
+//        } catch (IOException ioe) {
+//
+//        }
+//    }
 }
