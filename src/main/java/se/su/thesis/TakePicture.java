df8ed90357f6f431e2f41b160e986fabe8de725c
Joakim Berglund <joakim.berglund@hm.com>
2017-02-02 13:37:28 +0100

    implement cropping

--- src/main/java/se/su/thesis/TakePicture.java
+++ src/main/java/se/su/thesis/TakePicture.java
@@ -2,6 +2,7 @@ package se.su.thesis;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import net.coobird.thumbnailator.Thumbnails;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -19,6 +20,9 @@ public class TakePicture {
                 Controller.personLabelMap.get(Controller.currentPerson) +
                 "-" + Controller.currentPerson.toLowerCase() + "_" + i + ".png");
         BufferedImage bImage = SwingFXUtils.fromFXImage(image, null);
+        bImage = cropImage(bImage);
+        if (bImage == null)
+            System.err.println("Image crop failed");
         System.err.println("Image width: " + bImage.getWidth() + " Image height: " + bImage.getHeight());
         try {
             ImageIO.write(bImage, "png", outputFile);
@@ -26,5 +30,14 @@ public class TakePicture {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private BufferedImage cropImage(BufferedImage image) {
+        try {
+            return Thumbnails.of(image).size(150, 150).asBufferedImage();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
