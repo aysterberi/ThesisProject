43d03e26e97b04f74cc4936207c98847e8f320aa
Joakim Berglund <joakim.berglund@hm.com>
2017-02-02 14:24:23 +0100

    add method for populating map with persons on startup

--- src/main/java/se/su/thesis/Main.java
+++ src/main/java/se/su/thesis/Main.java
@@ -7,13 +7,35 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.opencv.core.Core;
 
+import java.io.File;
+
 public class Main extends Application {
 
+    public static final String PERSONS_DIRECTORY = "src/main/resources/persons/";
+
+    private void populateMap() {
+        File[] persons = new File(PERSONS_DIRECTORY).listFiles(File::isDirectory);
+        if (persons != null) {
+            for (File f : persons) {
+                Controller.personLabelMap.putIfAbsent(f.getName(), getPersonLabel(f.getName()));
+            }
+        }
+    }
+
+    private int getPersonLabel(String name) {
+        String path = PERSONS_DIRECTORY + name;
+        File[] files = new File(path).listFiles();
+        if (files != null)
+            return Integer.parseInt(files[0].getName().substring(0, 1));
+        return 0;
+    }
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/gui.fxml"));
         primaryStage.setTitle("Facial recognition");
         primaryStage.setScene(new Scene(root, 1024, 768));
+        populateMap();
         primaryStage.show();
     }
 
