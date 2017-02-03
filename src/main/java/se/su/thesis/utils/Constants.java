a26df9ebf889302d213866c29ba22484c4e867dd
Joakim Berglund <joakim.berglund@hm.com>
2017-02-03 08:39:33 +0100

    add constants

--- src/main/java/se/su/thesis/utils/Constants.java
+++ src/main/java/se/su/thesis/utils/Constants.java
@@ -3,4 +3,6 @@ package se.su.thesis.utils;
 public class Constants {
     public static final String LABEL_TEXT = "Current Person: ";
     public static final String PERSONS_DIRECTORY = "src/main/resources/persons/";
+    public static final String TEST_DIRECTORY = "src/main/resources/test/";
+    public static final String PNG_FORMAT = "png";
 }
