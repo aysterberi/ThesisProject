167d5015cc60a119f87e53df5f01b26136f5ed6e
Mattin Lotfi <matt@lotfi.se>
2017-02-05 12:56:14 +0100

    changed constats so it is .png

--- src/main/java/se/su/thesis/utils/Constants.java
+++ src/main/java/se/su/thesis/utils/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
     public static final String LABEL_TEXT = "Current Person: ";
     public static final String PERSONS_DIRECTORY = "src/main/resources/persons/";
     public static final String TEST_DIRECTORY = "src/main/resources/test/";
-    public static final String PNG_FORMAT = "png";
+    public static final String PNG_FORMAT = ".png";
 }
