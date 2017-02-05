a0e06935d50f9da7c8ce21ea7b1373748a76aa68
Mattin Lotfi <matt@lotfi.se>
2017-02-05 16:23:19 +0100

    fixed my mistake

--- src/main/java/se/su/thesis/TakePicture.java
+++ src/main/java/se/su/thesis/TakePicture.java
@@ -21,7 +21,7 @@ public class TakePicture {
     }
 
     private void takeTestPicture(Image image) {
-        File outputFile = new File(TEST_DIRECTORY + Controller.currentPerson + "." + PNG_FORMAT);
+        File outputFile = new File(TEST_DIRECTORY + Controller.currentPerson + "."+PNG_FORMAT);
         try {
             writeFile(image, PNG_FORMAT, outputFile);
             System.err.println(outputFile.getAbsolutePath());
@@ -36,7 +36,7 @@ public class TakePicture {
         File outputFile = new File(PERSONS_DIRECTORY +
                 Controller.currentPerson + "/" +
                 Controller.personLabelMap.get(Controller.currentPerson) +
-                "-" + Controller.currentPerson.toLowerCase() + "_" + i + "." + PNG_FORMAT);
+                "-" + Controller.currentPerson.toLowerCase() + "_" + i+ "." + PNG_FORMAT);
         try {
             writeFile(image, PNG_FORMAT, outputFile);
             System.err.println(outputFile.getAbsolutePath());
