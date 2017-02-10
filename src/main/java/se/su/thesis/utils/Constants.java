825d516ad1c716f12959dc9085faca617788f4dc
Joakim Berglund <joakim.berglund@hm.com>
2017-02-10 08:33:31 +0100

    add constant for image size

--- src/main/java/se/su/thesis/utils/Constants.java
+++ src/main/java/se/su/thesis/utils/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String TEST_DIRECTORY = "src/main/resources/test/";
     public static final String PNG_FORMAT = "png";
     public static final double RECOGNIZER_THRESHOLD = 3000.0;
+    public static final int IMAGE_SIZE = 300;
 }
