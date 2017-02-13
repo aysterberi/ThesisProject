7597cb1b6c51c42bd7d9bb9a859d5f74b9f4e5d3
matt <matt@lotfi.se>
2017-02-13 15:27:13 +0100

    #6, data do get saved now but have to manually remove saved data when something new is added

--- src/main/java/se/su/thesis/Recognizer.java
+++ src/main/java/se/su/thesis/Recognizer.java
@@ -10,7 +10,7 @@ import org.bytedeco.javacpp.opencv_core.Mat;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.nio.IntBuffer;
-import java.util.HashMap;
+import java.util.Map;
 
 import static org.bytedeco.javacpp.opencv_core.CV_32SC1;
 import static org.bytedeco.javacpp.opencv_core.MatVector;
@@ -18,12 +18,12 @@ import static org.bytedeco.javacpp.opencv_face.FaceRecognizer;
 import static org.bytedeco.javacpp.opencv_face.createEigenFaceRecognizer;
 import static org.bytedeco.javacpp.opencv_imgcodecs.CV_LOAD_IMAGE_GRAYSCALE;
 import static org.bytedeco.javacpp.opencv_imgcodecs.imread;
+import static se.su.thesis.utils.Constants.PERSON_SAVE_DATA;
 import static se.su.thesis.utils.Constants.RECOGNIZER_THRESHOLD;
 
 public class Recognizer {
     private int predictedLabel;
-    private HashMap<Integer, String> personsMap;
-
+    private FaceRecognizer faceRecognizer = createEigenFaceRecognizer(10, RECOGNIZER_THRESHOLD);
 
 
     public Recognizer() {
@@ -36,8 +36,18 @@ public class Recognizer {
 //        Mat testImage = new Mat(test.getHeight(),test.getWidth(), CV_LOAD_IMAGE_GRAYSCALE);
 
     public void recognize(String pathToTestImage) {
+        File personsdata = new File(PERSON_SAVE_DATA);
         Mat testImage = imread(pathToTestImage, CV_LOAD_IMAGE_GRAYSCALE);
-        personsMap = new HashMap<>();
+        if (personsdata.exists()){
+            System.out.println("loading data");
+            faceRecognizer.load(PERSON_SAVE_DATA);
+        }else {
+            trainOnPictures();
+        }
+            predictedLabel = faceRecognizer.predict(testImage);
+    }
+
+    private void trainOnPictures() {
         File[] directories = Controller.getExistingPersons();
         FilenameFilter imageFilter = (dir, name) -> {
             name = name.toLowerCase();
@@ -51,7 +61,6 @@ public class Recognizer {
         for (File dirs : directories) {
             System.out.println("Going through " + dirs.getName() + " folder");
             File root = new File(dirs.getAbsolutePath());
-//            File root = new File(pathToTrainingDirectory);
             File[] imageFiles = root.listFiles(imageFilter);
             IntBuffer labelsBuffer = labels.createBuffer();
 
@@ -61,13 +70,10 @@ public class Recognizer {
                 images.put(counter, image);
                 labelsBuffer.put(counter, label);
                 counter++;
-                personsMap.put(label, dirs.getName());
             }
-
         }
-            FaceRecognizer faceRecognizer = createEigenFaceRecognizer(10, RECOGNIZER_THRESHOLD);
-            faceRecognizer.train(images, labels);
-            predictedLabel = faceRecognizer.predict(testImage);
+        faceRecognizer.train(images, labels);
+        faceRecognizer.save(PERSON_SAVE_DATA);
     }
 
     public int getPredictedLabel() {
@@ -78,10 +84,10 @@ public class Recognizer {
         if (predictedLabel == 0){
             return "Unknown";
         }
-        return personsMap.get(predictedLabel);
+        return getKeyFromValue(Controller.personLabelMap, predictedLabel).toString();
     }
 
-    public int calculateFileLength(File[] directories, FilenameFilter imageFilter) {
+    private int calculateFileLength(File[] directories, FilenameFilter imageFilter) {
         int size = 0;
         for (File dirs : directories) {
             File root = new File(dirs.getAbsolutePath());
@@ -89,5 +95,14 @@ public class Recognizer {
             size += imageFiles.length;
         }
         return size;
+    }
+
+    private static Object getKeyFromValue(Map hm, Object value) {
+        for (Object o : hm.keySet()) {
+            if (hm.get(o).equals(value)) {
+                return o;
+            }
+        }
+        return null;
     }
 }
