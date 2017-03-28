652bba8fb5e1f9d45a1e550c02823c9e3d53dd65
matt <matt@lotfi.se>
2017-03-28 15:59:02 +0200

    Made new copy method for copying testpicture

--- src/main/java/se/su/thesis/TakePicture.java
+++ src/main/java/se/su/thesis/TakePicture.java
@@ -10,12 +10,17 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+import static com.sun.deploy.cache.Cache.copyFile;
 import static se.su.thesis.utils.Constants.*;
 
 public class TakePicture {
 
     public TakePicture(Image image) {
         takeTestPicture(image);
+    }
+
+    public TakePicture(String filePath, String currentPerson){
+        copyTestPictureToPerson(filePath, currentPerson);
     }
 
     public TakePicture(Image image, int pictureNumber, int labelNumber, ImageType type) throws IOException {
@@ -39,6 +44,25 @@ public class TakePicture {
                 Controller.currentPerson + "/" +
                 Controller.personLabelMap.get(Controller.currentPerson) +
                 "-" + Controller.currentPerson.toLowerCase() + "_" + pictureNumber + "." + PNG_FORMAT);
+        nameFile(image, outputFile);
+    }
+
+
+    private void copyTestPictureToPerson(String filePath, String currentPerson) {
+        File source=new File(filePath);
+        File outputFile = new File(PERSONS_DIRECTORY +
+                currentPerson + "/" +
+                Controller.personLabelMap.get(currentPerson) +
+                "-" + currentPerson.toLowerCase() + "_" + Controller.getPictureNumber
+                (PERSONS_DIRECTORY + currentPerson + "/") + "." + PNG_FORMAT);
+        try {
+            copyFile(source,outputFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void nameFile(Image image, File outputFile) {
         try {
             writeFile(image, PNG_FORMAT, outputFile);
             System.err.println(outputFile.getAbsolutePath());
